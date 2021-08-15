2313c2dd991206ca04924206a4454c4d74bc67f2
ibrahim <dnzibrahim1@gmail.com>
2021-08-15 13:53:17 +0300

    Add sass package

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
+<div>
+  <nuxt-logo />
   <Tutorial/>
+</div>
 </template>
 
 <script>
-export default {}
+import NuxtLogo from '~/components/NuxtLogo.vue'
+export default {
+  components: { NuxtLogo },}
 </script>
