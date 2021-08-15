c988a3ab5b067ef3856063bdb5457e4bdabfd095
ibrahim <dnzibrahim1@gmail.com>
2021-08-15 17:18:15 +0300

    Update build directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  buildDir: 'dist',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
