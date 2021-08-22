3e6a036c0d67f97825e174ee665a6021b5f69884
ibrahim <dnzibrahim1@gmail.com>
2021-08-22 15:01:28 +0300

    Add 500 response to resume routers for errors

--- api/resume.js
+++ api/resume.js
@@ -26,79 +26,101 @@ router.get('/desc', async (req, res) => {
     const result = response.paragraph.text[0].plain_text
     res.json({ data: result })
   } catch (error) {
-    res.json({ error })
+    res.status(500).send({ error })
   }
 })
 
 router.get('/personal-info', async (req, res) => {
   const databaseId = parseNotionId(NOTION_RESUME_INFO_ID)
-  const response = await notion.databases.query({
-    database_id: databaseId,
-    filter: {
-      or: [],
-    },
-  })
-  const personalInfos = response.results.map((result) => {
-    return {
-      type: result.properties?.Type?.title[0]?.plain_text || '',
-      value: result.properties?.Value?.rich_text[0]?.plain_text || '',
-    }
-  })
-  res.json({ data: personalInfos })
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        or: [],
+      },
+      sorts: [
+        {
+          property: 'Sort',
+          direction: 'ascending',
+        },
+      ],
+    })
+    const personalInfos = response.results.map((result) => {
+      return {
+        type: result.properties?.Type?.title[0]?.plain_text || '',
+        value: result.properties?.Value?.rich_text[0]?.plain_text || '',
+      }
+    })
+    res.json({ data: personalInfos })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
 })
 
 router.get('/experience', async (req, res) => {
   const databaseId = parseNotionId(NOTION_RESUME_EXP_ID)
-  const response = await notion.databases.query({
-    database_id: databaseId,
-    filter: {
-      or: [],
-    },
-  })
-  const personalInfos = response.results.map((result) => {
-    return {
-      where: getTableTextProperty(result, 'Where', { title: true }),
-      role: getTableTextProperty(result, 'Role'),
-      since: getTableTextProperty(result, 'Since'),
-      desc: getTableTextProperty(result, 'Description'),
-    }
-  })
-  res.json({ data: personalInfos })
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        or: [],
+      },
+    })
+    const personalInfos = response.results.map((result) => {
+      return {
+        where: getTableTextProperty(result, 'Where', { title: true }),
+        role: getTableTextProperty(result, 'Role'),
+        since: getTableTextProperty(result, 'Since'),
+        desc: getTableTextProperty(result, 'Description'),
+      }
+    })
+    res.json({ data: personalInfos })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
 })
 
 router.get('/education', async (req, res) => {
   const databaseId = parseNotionId(NOTION_RESUME_EDU_ID)
-  const response = await notion.databases.query({
-    database_id: databaseId,
-    filter: {
-      or: [],
-    },
-  })
-  const personalInfos = response.results.map((result) => {
-    return {
-      school: getTableTextProperty(result, 'School', { title: true }),
-      study: getTableTextProperty(result, 'Study'),
-      time: getTableTextProperty(result, 'Time'),
-    }
-  })
-  res.json({ data: personalInfos })
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        or: [],
+      },
+    })
+    const personalInfos = response.results.map((result) => {
+      return {
+        school: getTableTextProperty(result, 'School', { title: true }),
+        study: getTableTextProperty(result, 'Study'),
+        time: getTableTextProperty(result, 'Time'),
+      }
+    })
+    res.json({ data: personalInfos })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
 })
 
 router.get('/skills', async (req, res) => {
   const databaseId = parseNotionId(NOTION_RESUME_SKILLS_ID)
-  const response = await notion.databases.query({
-    database_id: databaseId,
-    filter: {
-      or: [],
-    },
-  })
-  const personalInfos = response.results.map((result) => {
-    return {
-      skillType: getTableTextProperty(result, 'SkillType', { title: true }),
-      skills: getTableSelectProperty(result, 'Skills'),
-    }
-  })
-  res.json({ data: personalInfos })
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        or: [],
+      },
+    })
+    const personalInfos = response.results.map((result) => {
+      return {
+        skillType: getTableTextProperty(result, 'SkillType', { title: true }),
+        skills: getTableSelectProperty(result, 'Skills'),
+      }
+    })
+    res.json({ data: personalInfos })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
 })
 
 module.exports = router
