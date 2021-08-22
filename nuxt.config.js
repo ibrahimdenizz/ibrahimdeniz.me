4d556289c9ceb82bbc84a1e34df841de15399502
ibrahim <dnzibrahim1@gmail.com>
2021-08-23 00:44:58 +0300

    Fix again base url issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,11 @@ export default {
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseUrl: process.env.VERCEL_URL
+      ? process.env.VERCEL_URL
+      : 'http://localhost:3000',
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
